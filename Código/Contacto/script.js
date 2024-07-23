2fe736f41dbd1477c5e39ac862c288c4de4453e2
Lautaro Lopez <70721690+Lautaro724@users.noreply.github.com>
2024-07-22 23:53:09 -0300

    Add files via upload

--- Código/Contacto/script.js
+++ Código/Contacto/script.js
@@ -1,12 +1,18 @@
 const evento = document.getElementById('button_send')
 const enviarFormulario = () => {
-	let nombre = document.getElementById('input_nombre').value;
-	let apellido = document.getElementById('input_apellido').value;
-	let mensaje = document.getElementById('mensaje').value;
+	let nombre = document.getElementById('input_nombre').value.trim();
+	let apellido = document.getElementById('input_apellido').value.trim();
+	let mensaje = document.getElementById('mensaje').value.trim();
 	let numero = 5493704555322;
-var win= window.open(`https://wa.me/${numero}?text=Hola%20mi%20nombre%20es%20${nombre}%20${apellido}, Asunto:%20${mensaje}`,'_blank');
+	//---------MENSAJE DE ALERTA AL NO COMPLETAR LOS INPUTS Y ENVIAR EL FORMULARIO---------//
+	if (nombre === "" || apellido === "" || mensaje === "") {
+        alert("Por favor, completa todos los campos antes de enviar el formulario.");
+        return;
+    }
+    //---------ENVIA LOS DATOS INTRODUCIDOS MEDIANTE EL USO DE WHATSAPP---------//
+	var win= window.open(`https://wa.me/${numero}?text=Hola%20mi%20nombre%20es%20${nombre}%20${apellido}, Asunto:%20${mensaje}`,'_blank');
 
 }
-
+//---------ABRE LA APLICACIÓN DE WHATSAPP EN UNA VENTANA---------//
 evento.addEventListener('click', enviarFormulario)
 
