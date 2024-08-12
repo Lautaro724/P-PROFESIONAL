81a3845261c049b3ee0a60a63189dfb41e2e8575
Lautaro Lopez <70721690+Lautaro724@users.noreply.github.com>
2024-08-12 16:03:33 -0300

    INTRODUCCION DE ANIMACIONES PARA TRANSICIONES

--- Código/Contacto/script.js
+++ Código/Contacto/script.js
@@ -34,3 +34,32 @@ const enviarFormulario = () => {
 evento.addEventListener('click', enviarFormulario)
 
 
+//---------------------------ANIMACIÓN PARA TRANSICIONAR DE UNA PÁGINA A OTRA--------------------------------// 
+function transitionToPage(href) {
+    gsap.to('body', {
+        opacity: 0,
+        duration: 0.5,
+        ease: 'power1.inOut',
+        onComplete: () => window.location.href = href
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    gsap.from('body', {
+        opacity: 0,
+        duration: 0.5,
+        ease: 'power1.inOut'
+    });
+
+    document.querySelectorAll('a.anim_transicion').forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const href = this.getAttribute('href');
+            transitionToPage(href);
+        });
+    });
+});
+
+
+
+
--- Código/Servicios/script.js
+++ Código/Servicios/script.js
@@ -10,3 +10,30 @@ var swiper = new Swiper('.swiper-container', {
   },
   loop: false, // REPITE DESDE EL PRINCIPIO CUANDO PASA EL ÚLTIMO SERVICIO
 });
+
+
+//---------------------------ANIMACIÓN PARA TRANSICIONAR DE UNA PÁGINA A OTRA-----------------------------//
+function transitionToPage(href) {
+    gsap.to('body', {
+        opacity: 0,
+        duration: 0.5,
+        ease: 'power1.inOut',
+        onComplete: () => window.location.href = href
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    gsap.from('body', {
+        opacity: 0,
+        duration: 0.5,
+        ease: 'power1.inOut'
+    });
+
+    document.querySelectorAll('a.anim_transicion').forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const href = this.getAttribute('href');
+            transitionToPage(href);
+        });
+    });
+});
